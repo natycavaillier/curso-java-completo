80f6333cd44be5dc0076e05d46528c390ce80e22
Natalia Cavaillier <nataliaabreu2000@hotmail.com>
2022-08-23 16:29:35 -0300

    FileReader e BufferedReader na mão grande

---
+++ src/arquivos/fileReaderAndBufferedReader/Program.java
@@ -0,0 +1,43 @@
+package arquivos.fileReaderAndBufferedReader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Program {
+    public static void main(String[] args) {
+
+        String path = "C:\\Users\\natal\\Documents\\in.txt";
+
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            fr = new FileReader(path);
+            br = new BufferedReader(fr);
+
+            // Lê uma linha e se o arquivo estiver no final, o arquivo retornará nulo
+            String line = br.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+                if (fr != null) {
+                    br.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
